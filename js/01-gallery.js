60e8dcbdde20aa29f00b6bf5d3b9294c9c68dec4
Golik07 <126725620+Golik07@users.noreply.github.com>
2023-07-04 21:53:19 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const fatherImages = document.querySelector(".gallery");
+const imgMarkUp = createImagesCards(galleryItems);
+
+fatherImages.insertAdjacentHTML("afterbegin", imgMarkUp);
+
+fatherImages.addEventListener("click", openImgs);
+
+function openImgs(event) {
+  event.preventDefault();
+  const imgValue = event.target.dataset.source;
+  const nodeNameImg = event.target.nodeName;
+
+  if (nodeNameImg !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+      <img src="${imgValue}" width="800" height="600">
+  `);
+  instance.show();
+
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function createImagesCards(imgs) {
+  return imgs
+    .map(({ preview, original, description }) => {
+      return `
+   <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const fatherImages = document.querySelector(".gallery");
+const imgMarkUp = onImagesCards(galleryItems);
+
+fatherImages.insertAdjacentHTML("afterbegin", imgMarkUp);
+
+fatherImages.addEventListener("click", openImgs);
+
+function openImgs(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function onImagesCards(imgs) {
+  return imgs
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+    </li>
+    `;
+    })
+    .join("");
+}
